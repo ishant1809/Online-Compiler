3ed55dc600089cd621e286ec137fff72e34af356
ishant1809 <162083108+ishant1809@users.noreply.github.com>
2025-01-26 23:50:07 +0530

    Initial project files

---
+++ Api.js
@@ -0,0 +1,104 @@
+const express = require("express")
+const app = express()
+const bodyP = require("body-parser")
+const compiler = require("compilex")
+const options = { stats: true }
+compiler.init(options)
+app.use(bodyP.json())
+app.use("/codemirror/codemirror-5.65.18", express.static("D:/0358AM/codemirror"));
+app.get("/", function (req, res) {
+    compiler.flush(function () {
+        console.log("deleted")
+    })
+    res.setHeader("Content-Type", "text/html");
+    res.sendFile("D:/0358AM/index.html");
+    app.use(express.static("D:/0358AM"));
+
+})
+app.post("/compile", function (req, res) {
+    var code = req.body.code
+    var input = req.body.input
+    var lang = req.body.lang
+    try {
+
+        if (lang == "Cpp") {
+            if (!input) {
+                var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } }; // (uses g++ command to compile )
+                compiler.compileCPP(envData, code, function (data) {
+                    if (data.output) {
+                        res.send(data);
+                    }
+                    else {
+                        res.send({ output: "error" })
+                    }
+                });
+            }
+            else {
+                var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } }; // (uses g++ command to compile )
+                compiler.compileCPPWithInput(envData, code, input, function (data) {
+                    if (data.output) {
+                        res.send(data);
+                    }
+                    else {
+                        res.send({ output: "error" })
+                    }
+                });
+            }
+        }
+        else if (lang == "Java") {
+            if (!input) {
+                var envData = { OS: "windows" };
+                compiler.compileJava(envData, code, function (data) {
+                    if (data.output) {
+                        res.send(data);
+                    }
+                    else {
+                        res.send({ output: "error" })
+                    }
+                })
+            }
+            else {
+                //if windows  
+                var envData = { OS: "windows" };
+                //else
+                compiler.compileJavaWithInput(envData, code, input, function (data) {
+                    if (data.output) {
+                        res.send(data);
+                    }
+                    else {
+                        res.send({ output: "error" })
+                    }
+                })
+            }
+        }
+        else if (lang == "Python") {
+            if (!input) {
+                var envData = { OS: "windows" };
+                compiler.compilePython(envData, code, function (data) {
+                    if (data.output) {
+                        res.send(data);
+                    }
+                    else {
+                        res.send({ output: "error" })
+                    }
+                });
+            }
+            else {
+                var envData = { OS: "windows" };
+                compiler.compilePythonWithInput(envData, code, input, function (data) {
+                    if (data.output) {
+                        res.send(data);
+                    }
+                    else {
+                        res.send({ output: "error" })
+                    }
+                });
+            }
+        }
+    }
+    catch (e) {
+        console.log("error")
+    }
+})
+
+app.listen(8000)
